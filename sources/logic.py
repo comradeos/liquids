1f14a495dd32bd591db5393f6e08870c2b551630
Iaroslav Os <osmnbx@gmail.com>
2022-07-15 18:23:42 +0300

    renamed methods

--- sources/logic.py
+++ sources/logic.py
@@ -5,7 +5,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
-from data import Drink
+from data import Liquid
 
 
 @app.route('/')
@@ -13,64 +13,64 @@ def index():
     return 'liquids ver.1.0'
 
 
-@app.route('/drinks', methods=['GET'])
-def get_drinks():
-    drinks = Drink.query.all()
+@app.route('/liquids', methods=['GET'])
+def get_liquids():
+    liquids = Liquid.query.all()
     output = []
 
-    for drink in drinks:
+    for liquid in liquids:
         data = {
-            'name': drink.name,
-            'description': drink.description,
+            'name': liquid.name,
+            'description': liquid.description,
         }
         output.append(data)
-    return {'drinks': output}
+    return {'liquids': output}
 
 
-@app.route('/drinks/<id>', methods=['GET'])
-def get_drink(id):
-    drink = Drink.query.get_or_404(id)
-    return {'name': drink.name, 'description': drink.description}
+@app.route('/liquids/<id>', methods=['GET'])
+def get_liquid(id):
+    liquid = Liquid.query.get_or_404(id)
+    return {'name': liquid.name, 'description': liquid.description}
 
 
-@app.route('/drinks', methods=['POST'])
-def add_drink():
-    new_drink = Drink(name=request.json['name'], description=request.json['description'])
+@app.route('/liquids', methods=['POST'])
+def add_liquid():
+    new_liquid = Liquid(name=request.json['name'], description=request.json['description'])
     try:
-        db.session.add(new_drink)
+        db.session.add(new_liquid)
         db.session.commit()
-        return {'id': new_drink.id}
+        return {'id': new_liquid.id}
     except:
         return {'error': 'unique name required'}
 
 
-@app.route('/drinks/delete/<id>', methods=['DELETE'])
-def delete_drink(id):
-    drink = Drink.query.get(id)
-    if drink == None:
+@app.route('/liquids/delete/<id>', methods=['DELETE'])
+def delete_liquid(id):
+    liquid = Liquid.query.get(id)
+    if liquid == None:
         return {'error': 'not found'}
     try:
-        db.session.delete(drink)
+        db.session.delete(liquid)
         db.session.commit()
-        return {'response': f"{drink.name} has been deleted"}
+        return {'response': f"{liquid.name} has been deleted"}
     except:
         return {'error': 'error'}
 
 
-@app.route('/drinks/update/', methods=['PUT'])
-def update_drink():
+@app.route('/liquids/update/', methods=['PUT'])
+def update_liquid():
     id = request.json['id']
-    drink = Drink.query.get(id)
+    liquid = Liquid.query.get(id)
     
-    if drink == None:
+    if liquid == None:
         return {'error': 'not found'}
     
-    drink.name = request.json['name']
-    drink.description = request.json['description']
+    liquid.name = request.json['name']
+    liquid.description = request.json['description']
     
     try:
         db.session.commit()
-        return {'response': f"{drink.name} has been updated"}
+        return {'response': f"{liquid.name} has been updated"}
     except:
         return {'error': 'error'}
 
