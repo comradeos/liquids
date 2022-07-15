9936079409e006f35115153899793731f25bc209
Iaroslav Os <osmnbx@gmail.com>
2022-07-15 14:28:38 +0300

    upd: reorg data/logic/main

--- sources/main.py
+++ sources/main.py
@@ -1,20 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
-
-        
-
-@app.route('/')
-def index():
-    return 'liquids ver.1.0'
-
-@app.route('/drinks')
-def drinks():
-    return {
-        "drinks":"drink data",
-    }
+from logic import app
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
